b7c5762137f4b001d717ab85b8450b085f5c77a3
mateusfelixdias <felixmateus618@gmail.com>
2023-01-02 07:02:33 -0300

    feat: added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useMemo } from 'react';
+import { Auth } from './pages/auth';
+import { Home } from './pages/home';
+import { themeSettings } from './themes';
+import { useSelector } from 'react-redux';
+import { Profile } from './pages/profile';
+import { createTheme } from '@mui/material/styles';
+import { InitialState } from './interfaces/initialState';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './styles/index.css';
 
 export const App = () => {
-  return <h1>Hello!</h1>;
+  const mode = useSelector((state: InitialState) => state.mode);
+  const correctedMode = mode !== 'dark' ? 'light' : 'dark';
+
+  const theme = useMemo(
+    () => createTheme(themeSettings(correctedMode)),
+    [correctedMode]
+  );
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 };
