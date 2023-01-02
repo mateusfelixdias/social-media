71e79dcc0ea60005d19ffc83c50d370d0ebef484
mateusfelixdias <felixmateus618@gmail.com>
2023-01-02 07:05:03 -0300

    feat: added redux config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import { App } from './App';
+import { reducer } from './state';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
+import { PersistGate } from 'redux-persist/integration/react';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  REHYDRATE,
+  persistStore,
+  persistReducer,
+  PURGE,
+} from 'redux-persist';
+
+const persistConfig = { key: 'root', storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE],
+      },
+    });
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
