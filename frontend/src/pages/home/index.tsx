5ee7d4d2c1bbe0be225653884fa6b1760a10651b
mateusfelixdias <felixmateus618@gmail.com>
2023-01-27 20:45:26 -0300

    feat: added new components in home page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,8 +1,51 @@
-import { Box } from "@mui/material";
-import { Navbar } from "../../scenes/navbar";
+import { useSelector } from 'react-redux';
+import { Navbar } from '../../scenes/navbar';
+import { Box, useMediaQuery } from '@mui/material';
+import { Posts } from '../../scenes/widgets/Posts';
+import { MyPost } from '../../scenes/widgets/MyPost';
+import { Advert } from '../../scenes/widgets/Advert';
+import { FriendList } from '../../scenes/widgets/FriendList';
+import { InitialState } from '../../interfaces/initialState';
+import { UserWidget } from '../../scenes/widgets/UserWidget';
 
 export const Home = () => {
-  return <Box>
-    <Navbar />
-  </Box>
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+  const { _id, picturePath } = useSelector((state: InitialState) => state.user);
+
+  return (
+    <Box>
+      <Navbar />
+
+      <Box
+        width="100%"
+        gap="0.5rem"
+        padding="2rem 6%"
+        justifyContent="space-between"
+        display={isNonMobileScreens ? 'flex' : 'block'}
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+
+        <Box
+          mt={isNonMobileScreens ? undefined : '2rem'}
+          flexBasis={isNonMobileScreens ? '42%' : undefined}
+        >
+          <MyPost picturePath={picturePath} />
+
+          <Posts userId={_id} />
+        </Box>
+
+        {isNonMobileScreens ? (
+          <Box flexBasis="26%">
+            <Advert />
+
+            <Box m="2rem 0" />
+
+            <FriendList userId={_id} />
+          </Box>
+        ) : null}
+      </Box>
+    </Box>
+  );
 };
