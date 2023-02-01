39eafafb90d180925f0d85278a447155d677f135
mateusfelixdias <felixmateus618@gmail.com>
2023-02-01 10:57:58 -0300

    feat: added improvements to authentication page

--- frontend/src/pages/auth/index.tsx
+++ frontend/src/pages/auth/index.tsx
@@ -1,11 +1,11 @@
 import { Form } from './steps/Form';
 import { useSelector } from 'react-redux';
 import { themeSettings } from '../../themes';
+import { Box, Typography } from '@mui/material';
+import { Header } from '../../components/Header';
 import { InitialState } from '../../interfaces/initialState';
-import { Box, Typography, useMediaQuery } from '@mui/material';
 
 export const Auth = () => {
-  const isNonMobileScreens = useMediaQuery('min-width: 1000px');
   const mode = useSelector((state: InitialState) => state.mode);
 
   const theme = themeSettings(mode);
@@ -13,24 +13,25 @@ export const Auth = () => {
 
   return (
     <Box>
-      <Box width="100%" bgcolor={alt} p="1rem 6%" textAlign="center">
-        <Typography
-          color="primary"
-          fontWeight="bold"
-          fontSize="32px"
-          borderRadius="1.5rem"
-          bgcolor={alt}
-        >
-          Mídia social
-        </Typography>
-      </Box>
+      <Header />
 
-      <Box p="2rem" m="2rem auto" width={isNonMobileScreens ? '50%' : '93%'}>
-        <Typography variant="h5" fontWeight="500" sx={{ mb: '1.5rem' }}>
-          Bem-vindo ao mídia social ;)
+      <Box
+        p="2rem"
+        width="93%"
+        m="2rem auto"
+        bgcolor={alt}
+        borderRadius="1.5rem"
+      >
+        <Typography
+          variant="h5"
+          fontWeight="500"
+          sx={{ mt: '1.5rem', mb: '1.5rem' }}
+        >
+          Bem-vindo(a) ao social mídia!
         </Typography>
+
+        <Form />
       </Box>
-      <Form />
     </Box>
   );
 };
